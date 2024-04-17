c7d88386e01ad313d23e3861a3d673b98aaf39fe
ygaurkar13 <2022bit021@sggs.ac.in>
2024-04-17 11:40:18 +0530

    Exam preparation

---
+++ Java_Practice/P6.java
@@ -0,0 +1,15 @@
+class Qwer{
+	public Qwer(){
+		System.out.println("Object Created");
+	}
+	public void show(){
+		System.out.println("In a show");
+	}
+}
+class P6{
+	public static void main(String ...args){
+		//Qwer obj;
+		new Qwer().show();
+		
+	}
+}
